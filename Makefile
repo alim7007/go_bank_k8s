d68bd079e0b7c420b100023b526b600cb134f80e
alim7007 <olimtoni7007@gmail.com>
2023-03-29 17:39:05 +0300

    working with db [POSTGRES]

---
+++ Makefile
@@ -0,0 +1,15 @@
+postgres:
+	docker run --name pg-cont -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=olim123 -d postgres
+createdb:
+	docker exec -it pg-cont createdb --username=root --owner=root olim_bank
+dropdb:
+	docker exec -it pg-cont dropdb olim_bank
+migrateup:
+	migrate -path db/migration -database "postgresql://root:olim123@localhost:5432/olim_bank?sslmode=disable" -verbose up
+migratedown:
+	migrate -path db/migration -database "postgresql://root:olim123@localhost:5432/olim_bank?sslmode=disable" -verbose down
+sqlc:
+	sqlc generate
+test:
+	go test -v -cover ./...
+.PHONY: postgres createdb dropdb migrateup migratedown sqlc
---
+++ util/random.go
@@ -0,0 +1,42 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(7)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "RUB", "CAD"}
+	k := len(currencies)
+	return currencies[rand.Intn(k)]
+}
